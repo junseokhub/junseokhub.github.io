38272b1420f0f85c3cbbfeb5343e2a16ecf88eab
junseokhub <github.yam117@passinbox.com>
2025-08-24 20:20:55 +0900

    Fix: env

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const clientEnv = {
+    VITE_EMAILJS_SERVICE_ID: env.VITE_EMAILJS_SERVICE_ID,
+    VITE_EMAILJS_TEMPLATE_ID: env.VITE_EMAILJS_TEMPLATE_ID,
+    VITE_EMAILJS_PUBLIC_KEY: env.VITE_EMAILJS_PUBLIC_KEY,
+  };
 
   return defineConfig({
     plugins: [react()],
     base: "/",
     define: {
-      'process.env': env,
+      "process.env": JSON.stringify(clientEnv),
     },
   });
 };
